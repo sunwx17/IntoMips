bec111bb4ba870749acd34f81a48a39df9194449
孙维孝 <sunwx17@mails.tsinghua.edu.cn>
2019-10-16 16:18:24 +0800

    set up make environment only for my windows

--- testbench/cpu/testcases/ori0.s
+++ testbench/cpu/testcases/ori0.s
@@ -1,4 +1,9 @@
-ori $1, $0, 0x1100
-ori $2, $0, 0x0020
-ori $3, $0, 0xff00
-ori $4, $0, 0xffff
+    .org 0x0
+    .global _start
+    .set noat
+_start:
+    ori $1, $0, 0x1100  # $1=0x00001100
+    ori $2, $0, 0x0020  # $2=0x00000020
+    ori $3, $0, 0xff00  # $3=0x0000ff00
+    ori $4, $0, 0xffff  # $4=0x0000ffff
+    
