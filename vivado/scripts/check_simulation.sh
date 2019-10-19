2205e887d0f31e4d20f9e5595808bc0fc329a848
孙维孝 <sunwx17@mails.tsinghua.edu.cn>
2019-10-19 15:19:59 +0800

    fix a stupid bug

--- vivado/scripts/check_simulation.sh
+++ vivado/scripts/check_simulation.sh
@@ -1,6 +1,6 @@
 #from https://github.com/trivialmips/TrivialMIPS/blob/master/vivado/scripts/check_simulation.sh
 
-grep "Fail" intomips.sim/sim_1/behav/xsim
+grep "Fail" intomips.sim/sim_1/behav/xsim/simulate.log
 
 if [ $? -eq 1 ]; then
     echo "CPU simulation succeeded."
