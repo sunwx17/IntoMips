49152b1b2e9d208286594a8929fa0702b08df0ce
孙维孝 <sunwx17@mails.tsinghua.edu.cn>
2019-10-19 14:52:56 +0800

    try CI

---
+++ vivado/scripts/run_simulation.tcl
@@ -0,0 +1,16 @@
+# from https://github.com/trivialmips/TrivialMIPS/blob/master/vivado/scripts/run_simulation.tcl
+
+update_compile_order -fileset sources_1
+
+set simulations [get_fileset $env(SIMULATION)]
+
+if { [llength simulations] != 0} {
+	foreach sim $simulations {
+        update_compile_order -fileset $sim
+        launch_simulation -simset $sim
+        # make simulation complete
+        run all
+	}
+}
+
+exit
