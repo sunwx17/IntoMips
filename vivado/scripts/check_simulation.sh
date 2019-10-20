8aafcc41bc86863820aed14daf8c322256b8359b
zrx <2017010306@secoder.net>
2019-10-20 19:16:13 +0800

    fix a tiny bug in check_simulation.sh

--- vivado/scripts/check_simulation.sh
+++ vivado/scripts/check_simulation.sh
@@ -1,6 +1,6 @@
 #from https://github.com/trivialmips/TrivialMIPS/blob/master/vivado/scripts/check_simulation.sh
 
-grep "Fail" intomips.sim/sim_cpu/behav/xsim/simulate.log
+grep "Fail" intomips.sim/${SIMULATION}/behav/xsim/simulate.log
 
 if [ $? -eq 1 ]; then
     echo "$1 simulation succeeded."
