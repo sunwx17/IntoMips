12a470c958d82f67d41fcdc541bd16249759613c
zrx <2017010306@secoder.net>
2019-11-23 21:32:31 +0800

    vga supprt visible ascii #29

--- src/defines.svh
+++ src/defines.svh
@@ -48,6 +48,17 @@ typedef logic[18:0] Vga_addr_t;
 `define VGA_HSP     976 
 `define VGA_VFP     637
 `define VGA_VSP     643
+`define VGA_NORMAL_HSIZE        800
+`define VGA_NORMAL_VSIZE        592
+`define VGA_BLOCK_HSIZE         8
+`define VGA_BLOCK_VSIZE         16
+`define VGA_BLOCK_HNUM          100
+`define VGA_BLOCK_VNUM          37
+
+//typedef logic[127:0] Ascii_data_t;
+typedef logic[0:127] Ascii_data_t;
+typedef logic[11:0] Graphics_block_addr_t;
+typedef logic[9:0] Block_bit_addr_t;
 
 typedef logic[1:0]  Serial_mode_t;
 
