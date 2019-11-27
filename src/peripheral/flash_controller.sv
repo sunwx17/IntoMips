ba533d9c30b972bc06d6c948e05500e2bd9867c7
zrx <2017010306@secoder.net>
2019-11-27 13:20:08 +0800

    flash stall

--- src/peripheral/flash_controller.sv
+++ src/peripheral/flash_controller.sv
@@ -7,6 +7,8 @@ module flash_controller(
     input Bit_t         read_op,        //读信号 1为读
     input Word_t        bus_data_write, //总线向flash写入数据(TODO)
     output Word_t       bus_data_read,  //总线从flash读入数据
+    output Bit_t        bus_stall,
+
 
     output Flash_addr_t flash_a,        //Flash地址，a0仅在8bit模式有效，16bit模式无意义
     inout  Halfword_t   flash_d,        //Flash数据
@@ -35,17 +37,28 @@ state_t cur_state = IDLE;
 
 always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
+        flash_oe_n <= 1'b0;
+        inner_addr <= `ZERO_WORD;
         cur_state <= IDLE;
-        flash_oe_n <= 1'b1;
-        inner_addr <= 0;
-        bus_data_read <= 0;
-            //$display("in rst time = %t", $time);
+        bus_data_read <= `ZERO_WORD;
+
+        bus_stall <= 1'b0;
     end else begin
         case(cur_state)
             IDLE: begin
-                flash_oe_n <= read_op? 1'b0: 1'b1;
-                inner_addr <= read_op? bus_addr: 0;
-                cur_state <= read_op? READ_0: IDLE;
+                if (read_op) begin
+                    flash_oe_n <= 1'b0;
+                    inner_addr <= bus_addr;
+                    cur_state <= READ_0;
+
+                    bus_stall <= 1'b1;
+                end else begin
+                    flash_oe_n <= 1'b1;
+                    inner_addr <= `ZERO_WORD;
+                    cur_state <= IDLE;
+
+                    bus_stall <= 1'b0;
+                end
             end
             READ_0: begin
                 bus_data_read[15:0] <= flash_d;
@@ -56,6 +69,8 @@ always_ff @(posedge clk or posedge rst) begin
                 bus_data_read[31:16] <= flash_d;
                 flash_oe_n <= 1'b0;
                 cur_state <= IDLE;
+
+                bus_stall <= 1'b0;
             end
         endcase
     end
