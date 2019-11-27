34decf40059d86e9af4a7719dfbd9b90a3a384f8
zrx <2017010306@secoder.net>
2019-11-27 23:05:57 +0800

    16bit flash_controller

--- src/peripheral/flash_controller.sv
+++ src/peripheral/flash_controller.sv
@@ -3,11 +3,9 @@
 module flash_controller(
     input clk, rst,
 
-    input Flash_addr_t  bus_addr,       //总线地址
+    input Word_t        bus_addr,       //总线地址
     input Bit_t         read_op,        //读信号 1为读
-    input Word_t        bus_data_write, //总线向flash写入数据(TODO)
-    output Word_t       bus_data_read,  //总线从flash读入数据
-    output Bit_t        bus_stall,
+    output Word_t       bus_data_read,  //总线从flash读入数据 实际只有半字 两个半字拼在一起
 
 
     output Flash_addr_t flash_a,        //Flash地址，a0仅在8bit模式有效，16bit模式无意义
@@ -23,56 +21,42 @@ module flash_controller(
 assign flash_rp_n = 1'b1; //防止不必要延迟
 assign flash_vpen = 1'b1;
 assign flash_byte_n = 1'b1; //使用16bit模式(halfword)
-assign flash_ce_n = 1'b0; //可以?
-
 //不支持写
 assign flash_we_n = 1'b1;
 
-Flash_addr_t inner_addr; //存储读取的addr
-assign flash_a = inner_addr;
+enum {IDLE, READ} State_t;
+State_t cur_state;
 
-typedef enum {IDLE, READ_0, READ_1} state_t;
-
-state_t cur_state = IDLE;
-
-always_ff @(posedge clk or posedge rst) begin
+always_ff @ (posedge clk or posedge rst) begin
     if (rst) begin
-        flash_oe_n <= 1'b0;
-        inner_addr <= `ZERO_WORD;
-        cur_state <= IDLE;
-        bus_data_read <= `ZERO_WORD;
-
-        bus_stall <= 1'b0;
+        cur_state   <= IDLE;
+        flash_ce_n  <= 1'b1;
+        flash_oe_n  <= 1'b1;
+        flash_a     <= `ZERO_WORD; 
     end else begin
         case(cur_state)
             IDLE: begin
+                cur_state   <= IDLE;
+                flash_ce_n  <= 1'b1;
+                flash_oe_n  <= 1'b1;
+                flash_d     <= `ZERO_WORD;
                 if (read_op) begin
-                    flash_oe_n <= 1'b0;
-                    inner_addr <= bus_addr;
-                    cur_state <= READ_0;
-
-                    bus_stall <= 1'b1;
-                end else begin
-                    flash_oe_n <= 1'b1;
-                    inner_addr <= `ZERO_WORD;
-                    cur_state <= IDLE;
-
-                    bus_stall <= 1'b0;
+                    cur_state   <= READ;
+                    flash_ce_n  <= 1'b0;
+                    flash_oe_n  <= 1'b0;
+                    flash_a     <= bus_addr;
                 end
             end
-            READ_0: begin
-                bus_data_read[15:0] <= flash_d;
-                inner_addr <= bus_addr + 2'h2;
-                cur_state <= READ_1;
-            end
-            READ_1: begin
-                bus_data_read[31:16] <= flash_d;
-                flash_oe_n <= 1'b0;
-                cur_state <= IDLE;
+            READ: begin
+                cur_state   <= IDLE;
+                flash_ce_n  <= 1'b1;
+                flash_oe_n  <= 1'b1;
+                flash_a     <= `ZERO_WORD;
 
-                bus_stall <= 1'b0;
+                bus_data_read   <= {flash_d, flash_d};
             end
         endcase
     end
 end
+
 endmodule
